98fa149795c0f773ffa7baf1e27929dede9cd826
03102001 <82948329+03102001@users.noreply.github.com>
2026-06-27 09:22:32 +0700

    fix mixed content https force scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,25 +4,26 @@ namespace App\Providers;
 
 use App\Models\FlightStatus;
 use App\Observers\FlightStatusObserver;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        // 🔥 FIX MIXED CONTENT (INI YANG KURANG)
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Vite::prefetch(concurrency: 3);
+
         FlightStatus::observe(FlightStatusObserver::class);
     }
 }
